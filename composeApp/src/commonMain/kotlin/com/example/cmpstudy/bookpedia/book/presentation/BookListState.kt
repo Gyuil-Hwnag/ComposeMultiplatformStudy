1e9160137ed3df4bf1017e9996003d5bd8ee70f6
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-11 22:15:54 +0900

    feat. BookList & BookListItem 추가.

--- composeApp/src/commonMain/kotlin/com/example/cmpstudy/bookpedia/book/presentation/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/bookpedia/book/presentation/BookListState.kt
@@ -11,3 +11,33 @@ data class BookListState(
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
 )
+
+val DEFAULT_BOOK = Book(
+    id = "1",
+    title = "Book Title",
+    imageUrl = "https://test.com",
+    authors = listOf("Test Author"),
+    description = "Book Description",
+    languages = emptyList(),
+    firstPublishYear = "1996",
+    averageRating = 4.8765,
+    ratingCount = 5,
+    numPages = 100,
+    numEditions = 3
+)
+
+val DEFAULT_BOOKS = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book Title $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Test Author $it"),
+        description = "Book Description $it",
+        languages = emptyList(),
+        firstPublishYear = "1996",
+        averageRating = 4.8765,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
