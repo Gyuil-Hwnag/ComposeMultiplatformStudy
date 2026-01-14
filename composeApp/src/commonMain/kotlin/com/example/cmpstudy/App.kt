9b1792c9ae27a521b2ec774ce55ff2559952bffd
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-14 22:33:54 +0900

    feat. Koin injection 추가.

--- composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
@@ -2,27 +2,16 @@ package com.example.cmpstudy
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import com.example.cmpstudy.bookpedia.book.data.datasource.RemoteBookDataSourceImpl
-import com.example.cmpstudy.bookpedia.book.data.repository.BookRepositoryImpl
 import com.example.cmpstudy.bookpedia.book.presentation.BookListScreenRoot
 import com.example.cmpstudy.bookpedia.book.presentation.BookListViewModel
-import com.example.cmpstudy.bookpedia.core.data.HttpClientFactory
-import com.example.cmpstudy.location.LocationScreen
-import io.ktor.client.engine.HttpClientEngine
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun App(engine: HttpClientEngine) {
+fun App() {
     MaterialTheme {
+        val viewModel = koinViewModel<BookListViewModel>()
         BookListScreenRoot(
-            viewModel = remember { BookListViewModel(
-                repository = BookRepositoryImpl(
-                    remoteBookDataSource = RemoteBookDataSourceImpl(
-                        httpClient = HttpClientFactory.create(engine = engine)
-                    ),
-                )
-            ) },
+            viewModel = viewModel,
             onBookClick = {}
         )
     }
--- composeApp/src/jvmMain/kotlin/com/example/cmpstudy/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/cmpstudy/main.kt
@@ -1,27 +1,32 @@
 package com.example.cmpstudy
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import io.ktor.client.engine.okhttp.OkHttp
+import com.example.cmpstudy.bookpedia.di.initKoin
 
-fun main() = application {
-    // Size: 윈도우 앱의 사이즈
-    // Position: 윈도루 앱의 위치
-    val state = rememberWindowState(
-        size = DpSize(800.dp, 500.dp),
-        position = WindowPosition(300.dp, 300.dp)
-    )
-    Window(
-        title = "BookPedia",
-        onCloseRequest = ::exitApplication,
-        state = state,
-        alwaysOnTop = true
-    ) {
-        App(engine = remember { OkHttp.create() })
+fun main() {
+    // NOTE: Koin injection
+    initKoin()
+
+    application {
+        // Size: 윈도우 앱의 사이즈
+        // Position: 윈도루 앱의 위치
+        val state = rememberWindowState(
+            size = DpSize(800.dp, 500.dp),
+            position = WindowPosition(300.dp, 300.dp)
+        )
+        Window(
+            title = "BookPedia",
+            onCloseRequest = ::exitApplication,
+            state = state,
+            alwaysOnTop = true
+        ) {
+            App()
+        }
     }
+
 }
