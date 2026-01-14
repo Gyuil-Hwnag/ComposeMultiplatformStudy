63fa8505c71d41a0ff575ff76dc4584ba86f9f2e
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-14 22:44:30 +0900

    fix. 웹 빌드를 위한 koinViewModel 직접 주입 받도록 변경.

--- composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
@@ -2,14 +2,20 @@ package com.example.cmpstudy
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.example.cmpstudy.bookpedia.book.presentation.BookListScreenRoot
 import com.example.cmpstudy.bookpedia.book.presentation.BookListViewModel
+import org.koin.compose.getKoin
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App() {
     MaterialTheme {
-        val viewModel = koinViewModel<BookListViewModel>()
+        // NOTE: koinViewModel 이 WebMain 에서는 동작하지 않아서 koin.get() 으로 주입 받아야 함.
+        // val viewModel = koinViewModel<BookListViewModel>()
+
+        val koin = getKoin()
+        val viewModel = remember { koin.get<BookListViewModel>() }
         BookListScreenRoot(
             viewModel = viewModel,
             onBookClick = {}
