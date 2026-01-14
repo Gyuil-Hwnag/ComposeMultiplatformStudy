357459efb49c674d304b65630ab5262a70d69dde
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-14 23:10:03 +0900

    feat. Routes 세팅.

--- composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/App.kt
@@ -3,22 +3,42 @@ package com.example.cmpstudy
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import com.example.cmpstudy.bookpedia.book.presentation.BookListScreenRoot
-import com.example.cmpstudy.bookpedia.book.presentation.BookListViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import androidx.navigation.toRoute
+import com.example.cmpstudy.bookpedia.app.Routes
+import com.example.cmpstudy.bookpedia.book.presentation.list.BookListScreenRoot
+import com.example.cmpstudy.bookpedia.book.presentation.list.BookListViewModel
 import org.koin.compose.getKoin
-import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App() {
     MaterialTheme {
-        // NOTE: koinViewModel 이 WebMain 에서는 동작하지 않아서 koin.get() 으로 주입 받아야 함.
-        // val viewModel = koinViewModel<BookListViewModel>()
-
-        val koin = getKoin()
-        val viewModel = remember { koin.get<BookListViewModel>() }
-        BookListScreenRoot(
-            viewModel = viewModel,
-            onBookClick = {}
-        )
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = Routes.BookGraph
+        ) {
+            navigation<Routes.BookGraph>(
+                startDestination = Routes.BookList
+            ) {
+                composable<Routes.BookList> {
+                    // NOTE: koinViewModel 이 WebMain 에서는 동작하지 않아서 koin.get() 으로 주입 받아야 함.
+                    // val viewModel = koinViewModel<BookListViewModel>()
+                    val koin = getKoin()
+                    val viewModel = remember { koin.get<BookListViewModel>() }
+                    BookListScreenRoot(
+                        viewModel = viewModel,
+                        onBookClick = { book -> navController.navigate(Routes.BookDetail(book.id)) }
+                    )
+                }
+ 
+                composable<Routes.BookDetail> { entry ->
+                    val args = entry.toRoute<Routes.BookDetail>()
+                }
+            }
+        }
     }
 }
