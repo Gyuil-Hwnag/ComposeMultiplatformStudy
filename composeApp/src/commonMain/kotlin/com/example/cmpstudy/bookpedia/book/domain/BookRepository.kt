c5d80537c338b3712d7331efc924945848d9d2f1
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-19 00:54:14 +0900

    feat. FavoriteRepository 연동.

--- composeApp/src/commonMain/kotlin/com/example/cmpstudy/bookpedia/book/domain/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/bookpedia/book/domain/BookRepository.kt
@@ -1,9 +1,16 @@
 package com.example.cmpstudy.bookpedia.book.domain
 
 import com.example.cmpstudy.bookpedia.core.domain.DataError
+import com.example.cmpstudy.bookpedia.core.domain.EmptyResult
 import com.example.cmpstudy.bookpedia.core.domain.Result
+import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
     suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
     suspend fun getBookDescription(bookId: String): Result<String?, DataError.Remote>
+
+    fun getFavoriteBooks(): Flow<List<Book>>
+    fun isBookFavorite(id: String): Flow<Boolean>
+    suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local>
+    suspend fun deleteFromFavorites(id: String)
 }
