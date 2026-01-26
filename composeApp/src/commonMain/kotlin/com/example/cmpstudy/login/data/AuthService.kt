e9099303749e16de956d9eb42694bb9494822038
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-01-26 22:40:03 +0900

    feat. Login > AuthService 추가.

---
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/login/data/AuthService.kt
@@ -0,0 +1,14 @@
+package com.example.cmpstudy.login.data
+
+import com.example.cmpstudy.login.domain.User
+import kotlinx.coroutines.flow.Flow
+
+interface AuthService {
+    val currentUserId: String
+    val isAuthenticated: Boolean
+    val currentUser: Flow<User>
+
+    suspend fun authenticate(email: String, password: String)
+    suspend fun createUser(email: String, password: String)
+    suspend fun signOut()
+}
---
+++ composeApp/src/commonMain/kotlin/com/example/cmpstudy/login/data/AuthServiceImpl.kt
@@ -0,0 +1,50 @@
+package com.example.cmpstudy.login.data
+
+import com.example.cmpstudy.login.domain.User
+import dev.gitlive.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AuthServiceImpl(
+    val auth: FirebaseAuth,
+    val scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+) : AuthService {
+
+    override val currentUserId: String
+        get() = auth.currentUser?.uid.toString()
+
+    override val isAuthenticated: Boolean
+        get() = auth.currentUser != null && auth.currentUser?.isAnonymous == false
+
+    override val currentUser: Flow<User> = auth.authStateChanged.map { user ->
+        user?.let { User(user.uid, user.isAnonymous) } ?: User()
+    }
+
+    private suspend fun launchWithAwait(block: suspend () -> Unit) {
+        scope.async { block() }.await()
+    }
+
+    override suspend fun authenticate(email: String, password: String) {
+        launchWithAwait {
+            auth.signInWithEmailAndPassword(email, password)
+        }
+    }
+
+    override suspend fun createUser(email: String, password: String) {
+        val result = launchWithAwait {
+            auth.createUserWithEmailAndPassword(email, password)
+        }
+    }
+
+    override suspend fun signOut() {
+        val isAnonymous = auth.currentUser?.isAnonymous == true
+        if (isAnonymous) {
+            auth.currentUser?.delete()
+        }
+        auth.signOut()
+    }
+}
