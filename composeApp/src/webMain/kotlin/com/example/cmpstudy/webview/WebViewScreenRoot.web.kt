c2ceae2056c4e700eb9aba773117e3c40fc07f05
Gyuil-Hwnag <hwanggyuil@gmail.com>
2026-02-06 00:09:22 +0900

    fix. WebView > Web에서는 리다이렉팅 하도록 변경.

--- composeApp/src/webMain/kotlin/com/example/cmpstudy/webview/WebViewScreenRoot.web.kt
+++ composeApp/src/webMain/kotlin/com/example/cmpstudy/webview/WebViewScreenRoot.web.kt
@@ -1,12 +1,15 @@
 package com.example.cmpstudy.webview
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.browser.window
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 actual fun WebViewScreenRoot() {
-    MaterialTheme {
-        Text("WebView is not supported on Web platform")
+    val url = "https://github.com"
+    LaunchedEffect(Unit) {
+        window.location.href = url
     }
 }
